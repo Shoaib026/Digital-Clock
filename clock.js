cc11a30f9ffd93f9fa6eabf718e7a5b902193e15
Shoaib <101673132+Shoaib026@users.noreply.github.com>
2024-02-24 02:16:18 +0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,11 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+
+setInterval(()=>{
+    let currentTime = new Date();
+    hrs.innerHTML = currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds();
+},1000);
